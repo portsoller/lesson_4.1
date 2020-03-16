7f4440bedd4247b5efccdcc5086d8a57e0b1ec51
Aleksandr Abramenko <portsoller@gmail.com>
2020-03-16 23:52:12 +0200

    Added assertation to the login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,23 +1,30 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+        # self.should_be_login_form()
+        # self.should_be_register_form()
 
     def should_be_login_url(self):
         self.should_be_login_url()
-        # реализуйте проверку на корректный url адрес
+        # assert self.is_element_present(By.CSS_SELECTOR, "#login_link_error"), "Login link is not presented"
+        assert self.current_url("login"), "Login is not presented"
         assert True
 
     def should_be_login_form(self):
         self.should_be_login_form()
-        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(By.CSS_SELECTOR, "#id_login-username_error"), "Login input is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#id_login-password_error"), "Password input is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_form > button_error"), "Login button is not presented"
         assert True
 
     def should_be_register_form(self):
         self.should_be_register_form()
-        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(By.CSS_SELECTOR, "#id_registration-email_error"), "Register input is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#id_registration-password1_error"), "Password input is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#id_registration-password2_error"), "Second password input is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#register_form > button_error"), "Register button is not presented"
         assert True
