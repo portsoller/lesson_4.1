e641b94a1868e313b070d353cea10bb0e87135b3
Aleksandr Abramenko <portsoller@gmail.com>
2020-03-17 00:21:04 +0200

    initialization LoginPage in body of test

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,12 +5,9 @@ from .locators import LoginPageLocators
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
-        # self.should_be_login_form()
-        # self.should_be_register_form()
 
     def should_be_login_url(self):
         self.should_be_login_url()
-        # assert self.is_element_present(By.CSS_SELECTOR, "#login_link_error"), "Login link is not presented"
         assert self.current_url("login"), "Login is not presented"
         assert True
 
